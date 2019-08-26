4a870fb096e25839c1ed888fa6d0cac24939007a
K0d1Lu <corsinil@ntymail.com>
2019-08-26 22:47:36 +0200

    create film item component

---
+++ components/filmtems.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native'
+import { getImageFromApi } from '../api/TMDBApi'
+
+export default class filmItem extends Component {
+  render() {
+    const { film } = this.props;
+    return (
+      <View style={styles.main_container}>
+        <Image
+          style={styles.image}
+          source={{uri: getImageFromApi(film.poster_path) }}
+        />
+        <View style={styles.info_container}>
+          <View style={styles.header_container}>
+            <Text style={styles.title_text}>{film.title}</Text>
+            <Text style={styles.vote_text}>{film.vote_average}</Text>
+          </View>
+            <Text style={styles.overview} numberOfLines={6}>{film.overview}</Text>
+            <Text style={styles.release}>{film.release_date}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    height: 190,
+    flexDirection: "row",
+    borderColor: '#000000',
+    borderWidth: 1,
+    margin: 10
+  },
+  image: {
+    width: 120,
+    height: 180,
+    margin: 5
+  },
+  info_container: {
+    flex: 1,
+    margin: 5
+  },
+  header_container: {
+    flexDirection: "row",
+    flex: 3,
+    height: 40
+  },
+  title_text: {
+    flex: 1,
+    flexWrap: 'wrap',
+    paddingRight: 5,
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  vote_text: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#666666'
+  },
+  release: {
+    textAlign: 'right',
+    fontSize: 14
+  }
+})
