fefefd16f714bf4f5396ae07cd2640cf21eb0ac0
K0d1Lu <corsinil@ntymail.com>
2019-08-26 22:47:01 +0200

    use env

---
+++ environment.js
@@ -0,0 +1,10 @@
+const ENV = {
+ apiBaseUrl: 'https://api.themoviedb.org/',
+ APIKEY: '3a86bf9565c44daf5109deddbd6d2627'
+};
+
+const getEnvVars = () => {
+ return ENV;
+};
+
+export default getEnvVars;
